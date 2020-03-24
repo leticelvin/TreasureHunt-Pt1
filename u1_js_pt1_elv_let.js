141aa765b74a8a0e0e0fae3c220a97b67ec9ff0a
leticelvin <54833746+leticelvin@users.noreply.github.com>
2020-03-24 20:10:21 +0100

    100% Finished!

--- u1_js_pt1_elv_let.js
+++ u1_js_pt1_elv_let.js
@@ -10,68 +10,71 @@ points = points - 5;
 return points;
 }
 
+var chest1 = "x";
+var chest2 = "x";
+var chest3 = "x";
 var chests = [chest1, chest2, chest3];
-var chest1 = "empty";
-var chest2 = "empty";
-var chest3 = "empty";
-console.log(chests);
+var concealedchests = [chest1, chest2, chest3];
 var z;
-
 z = Math.floor((Math.random() * 3) + 1);
-console.log(z);
 
 //chestshuffle puts the diamond in a random chest
 function chestshuffle(z) {
-    switch(z) {
-     case (z == 1): {
-        chest1 = "hasdiamond";
-     }
-     case (z == 2): {
-        chest2 = "hasdiamond";
-     }
-     case (z == 3): {
-        chest3 = "hasdiamond";
-     }     
+   switch(z) {
+     case 1:
+        chests[0] = "v";
+        chests[1] = "x";
+        chests[2] = "x";
+      break;
+     case 2:
+        chests[0] = "x";
+        chests[1] = "v";
+        chests[2] = "x";
+      break;
+     case 3: 
+        chests[0] = "x";
+        chests[1] = "x";
+        chests[2] = "v";
+      break;
+      default:
+      break;          
    }
-   console.log(z);
    return z;
-}
 
+}
 //chestshuffle();
 
 setTimeout(function() {
 for (i=0; i<1; i++) {
 console.log("-----------------------")
 console.log("Time to guess! Pick a chest from 1-3")
-console.log(chests)
+console.log(chests) //Matches the video
+console.log(z);
 } 1000});
 
 //different decisions depending on what number the user chooses by typing in openchest(number)
 function guess(x) {
    chestshuffle(z);
-   console.log(arguments[0]);
-   console.log("the number for z is " + z);
-   switch(x){
-      case z:
+   //console.log(arguments[0]);
+   if (x == z) {
          console.log(chests);
          console.log("Woohoo! You found the diamond");
          addpoints();
          console.log("Your score is "  + points);
          console.log("-----------------------");
          console.log("Time to guess! Pick a chest from 1-3")
-         console.log(chests);
-      break;
-      case !z:
+         console.log(concealedchests);
+         z = Math.floor((Math.random() * 3) + 1);
+         chestshuffle()
+      } else {
          console.log(chests);
          console.log("Muhaha! You picked the wrong chest...");
          removepoints();
          console.log("Your score is "  + points);
          console.log("-----------------------");
          console.log("Time to guess! Pick a chest from 1-3")
-         console.log(chests);
-      break;
-      default:
-         prompt("Invalid number.");
-      break;
+         console.log(concealedchests);
+         z = Math.floor((Math.random() * 3) + 1);
+         chestshuffle()
    } 
 }
